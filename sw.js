058d19187454b96b4a14a5c6caf381e5979b23ef
samsheybk2 <samireduardovasquezzambrano@gmail.com>
2026-06-07 20:57:08 -0400

    Add PWA support (manifest, service worker, meta tags)

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'elohim-v1';
+const URLS = ['index.html', 'manifest.json', 'favicon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
